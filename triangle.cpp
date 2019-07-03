17fa44ebaf34265e1f9bd36381e9b1dce4c54f47
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-03 00:45:25 -0700

    Change model, view and projection matrices to display the objects differently.

--- triangle.cpp
+++ triangle.cpp
@@ -98,6 +98,19 @@ int main(){
       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
   };
 
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+  };
+
   unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
@@ -198,6 +211,7 @@ int main(){
   ourShader.setInt("texture2", 1);
 
   glEnable(GL_DEPTH_TEST);
+  srand(glfwGetTime());
 
   while(!glfwWindowShouldClose(window))
   {
@@ -226,7 +240,7 @@ int main(){
     // Model matrix
     // ------------
     glm::mat4 model(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+    //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
 
     // View matrix
     // -----------
@@ -245,8 +259,17 @@ int main(){
     ourShader.setMatrix("projection", 1, 0, glm::value_ptr(projection));
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-
+    for(unsigned int i = 0; i < 10; i++) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * i;
+      float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+      if (i%2==0) {
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(r, r, r));
+      }
+      ourShader.setMatrix("model", 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
     // glBindVertexArray(0); // no need to unbind it every time
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
