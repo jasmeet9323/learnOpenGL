1820553418e8f78050a8179b3cf1dcab36730112
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-13 23:21:37 -0700

    Excercise: Constrain camera to xz plane.

--- camera.h
+++ camera.h
@@ -75,6 +75,8 @@ public:
       Position -= Right * velocity;
     if (direction == RIGHT)
       Position += Right * velocity;
+
+    Position.y = 0.0f;
   }
 
   // Processes input received from a mouse input system. Expects the offset
